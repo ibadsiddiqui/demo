5784cc1292950740948d8dec30aaec6ed560aa3e
ibad siddiqui <ibadsiddiqui@ibads-Mac-mini.local>
2018-11-29 12:49:31 +0500

    main index refactored to single class

--- index.js
+++ index.js
@@ -1,64 +1,46 @@
-import { Platform } from 'react-native'
+// import { Platform } from 'react-native'
+// import { Navigation } from 'react-native-navigation';
+
+// import { AppRegistry } from 'react-native';
+// import App from './App/App';
+// import { name as appName } from './app.json';
+
+
+
+// Navigation.events().registerAppLaunchedListener(() => {
+//   Navigation.setRoot({
+//     stack: {
+//       children: [{
+//         component: {
+//           name: 'SplashScreen',
+//         }
+//       }],
+//   options: {
+//     topBar: {
+//       title: 'Top bar Title'
+//     },
+//     visible: false
+//   }
+//     }
+//   })
+
+// });
+/** @format */
+
 import { Navigation } from 'react-native-navigation';
+import { registerScreens } from './src/screens';
 
-import { AppRegistry } from 'react-native';
-import App from './App/App';
-import { name as appName } from './app.json';
-
-
-import NoWifiScreen from './App/Containers/Screens/NoWifiScreen';
-import SplashScreenLoader from './App/Containers/Screens/SplashScreen';
-
-import ConfirmPINScreen from './App/Containers/Screens/ConfirmPINScreen';
-
-import LoginScreen from './App/Containers/Screens/LoginScreen'
-import RegistrationScreen from './App/Containers/Screens/RegisterationScreen'
-import RegistrationAgeAndGender from './App/Containers/Screens/Registration-AgeAndGender';
-import RegistrationCityAndEmail from './App/Containers/Screens/Registration-CityAndEmail';
-import RegistrationPasswordAndPhoneNumber from './App/Containers/Screens/Registration-PasswordAndPhoneNumber'
-import ForgetPasswordScreen from './App/Containers/Screens/ForgetPasswordScreen'
-
-import ProfileScreen from './App/Containers/Screens/ProfileScreen'
-import DashboardScreen from './App/Containers/Screens/DashboardScreen'
-
-Navigation.registerComponent('SplashScreen', () => SplashScreenLoader);
-Navigation.registerComponent('ConfirmPINScreen', () => ConfirmPINScreen); //done
-
-Navigation.registerComponent('Home', () => LoginScreen); //done
-Navigation.registerComponent('RegistrationScreen', () => RegistrationScreen); //done
-Navigation.registerComponent('AgeAndGender', () => RegistrationAgeAndGender); //done
-Navigation.registerComponent('CityAndEmail', () => RegistrationCityAndEmail);//done
-Navigation.registerComponent('PasswordAndPhoneNumber', () => RegistrationPasswordAndPhoneNumber); //done
-
-Navigation.registerComponent('ForgetPassword', () => ForgetPasswordScreen);// done
-
-Navigation.registerComponent('Dashboard', () => DashboardScreen);
-
-
-
-// Navigation.registerComponent('DashboardScreen', DashboardScreen);
-Navigation.registerComponent('ProfileScreen', () => ProfileScreen);
+registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'ConfirmPINScreen',
-            }
-          },
-          {
-            component: {
-              name: 'SplashScreen',
-            }
-          },
-
-        ]
+      component: {
+        name: 'Initializing'
       }
+    },
+    options: {
+      visible: false
     }
-  })
-})
-/** @format */
-
+  });
+});
